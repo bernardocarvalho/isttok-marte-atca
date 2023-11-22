fffa4bc0a99ef541fa24897656dec72860b47575
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-11-22 17:43:31 +0000

    Added MDS Python read script
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- marte2-exports.sh
+++ marte2-exports.sh
@@ -1,4 +1,4 @@
-# MARTe2 local envs for running/compiling MARTe2 Rt Apps
+# MARTe2 local envs for running/compiling MARTe2 ISTTOK ATCA node
 export TARGET=x86-linux
 export MARTe2_HOME=/opt/marte
 
@@ -13,13 +13,9 @@ export EPICS_CA_ADDR_LIST="localhost efda-marte.ipfn.tecnico.ulisboa.pt"
 export EPICS_CA_AUTO_ADDR_LIST="NO"
 
 export MDSPLUS_DIR=/usr/local/mdsplus
-export default_tree_path=$MARTe2_HOME/mdsplus-trees
-export rtappdemo_path=$MARTe2_HOME/mdsplus-trees
+#export default_tree_path=$MARTe2_HOME/mdsplus-trees
 export rtappisttok_path=$MARTe2_HOME/mdsplus-trees
 
 export PATH=$MDSPLUS_DIR/bin:$EPICS_BASE/bin/$EPICS_HOST_ARCH:$PATH
-#export SDN_CORE_INCLUDE_DIR=~/Projects/SDN_1.0.12_nonCCS/src/main/c++/include/
-#export SDN_CORE_LIBRARY_DIR=~/Projects/SDN_1.0.12_nonCCS/target/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_DIR/Build/x86-linux/Core/:$EPICS_BASE/lib/$EPICS_HOST_ARCH
-#:$SDN_CORE_LIBRARY_DIR
 
