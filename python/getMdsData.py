03ba3f374700d1dbd99678e2cb9667b273067cef
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-11-22 17:00:07 +0000

    Added MDS Python read script
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- python/getMdsData.py
+++ python/getMdsData.py
@@ -8,12 +8,34 @@ import matplotlib.pyplot as plt
 from MDSplus import Tree
 
 mdsTreeName = 'rtappisttok'
-#mdsPulseNumber = int(sys.argv[1])
-mdsPulseNumber = 1
+
+if len(sys.argv) > 1:
+    mdsPulseNumber = int(sys.argv[1])
+else:
+    mdsPulseNumber = 1
+    #treename = ''
 
 try:
     tree = Tree(mdsTreeName, mdsPulseNumber)
 except:
     print(f'Failed opening {mdsTreeName} for pulse number {mdsPulseNumber:d}')
     exit()
+
+if __name__ == '__main__':
+    mdsNode = tree.getNode("ATCAIOP1.ADC0RAW")
+    dataAdc = mdsNode.getData().data()
+    timeData = mdsNode.getDimensionAt(0).data()
+    plt.figure()
+    plt.plot(timeData, dataAdc, label=f"M{1} CHA")
+    mdsNode = tree.getNode("ATCAIOP1.ADC1RAW")
+    dataAdc = mdsNode.getData().data()
+    timeData = mdsNode.getDimensionAt(0).data()
+    plt.plot(timeData, dataAdc, label=f"M{1} CHB")
+    plt.legend()
+    plt.grid()
+    plt.ylabel("ADC counts")
+    plt.xlabel("Sample")
+    plt.show()
+
+
 
